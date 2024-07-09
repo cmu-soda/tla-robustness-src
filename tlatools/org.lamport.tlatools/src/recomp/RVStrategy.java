e9a3ae113290718fae2919abb21f7ab01debd2b4
eduardoloz <eddieslozano@gmail.com>
2024-07-09 15:35:19 -0400

    Added File RVStrategy to separate the implementation of the Runnable interface from recomp-verify. Note: the globalIsDone boolean will be changed to be a string that is shared (will be reppresentative of the final output produced by the program)

---
+++ tlatools/org.lamport.tlatools/src/recomp/RVStrategy.java
@@ -0,0 +1,51 @@
+package recomp;
+
+import java.util.List;
+
+public class RVStrategy implements Runnable {
+    public static boolean globalIsDone = false;
+
+    private String tla;
+    private String cfg;
+    private String recompType;
+    private String recompFile;
+    private boolean verbose;
+
+    public RVStrategy(String tla, String cfg, String recompType, String recompFile, boolean verbose) throws InterruptedException {
+        this.tla = tla;
+        this.cfg = cfg;
+        this.recompType = recompType;
+        this.recompFile = recompFile;
+        this.verbose = verbose;
+    }
+
+    synchronized static public boolean isThreadDone() {
+        boolean isDone = globalIsDone;
+        globalIsDone = true;
+        return isDone;
+    }
+
+    public void runRVStrategies(String tla, String cfg, String recompType, String recompFile, boolean verbose) throws InterruptedException {
+//		ArrayList<Thread> threads = new ArrayList<>();
+        RVStrategy sampleStrategy = new RVStrategy(tla, cfg, recompType, recompFile, verbose);
+        Thread thread = new Thread(sampleStrategy);
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            System.out.println("Thread interrupted while waiting on something.");
+        }
+
+    }
+
+    @ Override
+    public void run() {
+        try {
+            List<String> rawComponents = Decomposition.decompAll(tla, cfg);
+            RecompVerify.runRecompMap(tla, cfg, recompType, recompFile, verbose, rawComponents);
+        } catch (Exception e) {
+            System.out.println("Thread interrupted while waiting on something.");
+        }
+    }
+}
