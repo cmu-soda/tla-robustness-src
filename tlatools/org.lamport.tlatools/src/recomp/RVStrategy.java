5bb8afdfa6774b8a3fba3bade80e89353bbed300
eduardoloz <eddieslozano@gmail.com>
2024-07-11 13:54:27 -0400

    Added comments and addressed issues from pull request feedback. Additionally moved and renamed runRecompMap to runRecompStrategy (moved from RecompVerify.java to RVStrategy.java

--- tlatools/org.lamport.tlatools/src/recomp/RVStrategy.java
+++ tlatools/org.lamport.tlatools/src/recomp/RVStrategy.java
@@ -1,8 +1,20 @@
 package recomp;
 
+import cmu.isr.ts.LTS;
+import cmu.isr.ts.ParallelComposition;
+import cmu.isr.ts.lts.SafetyUtils;
+import lts.SymbolTable;
+import tlc2.AlphabetMembershipTester;
+import tlc2.TLC;
+import tlc2.Utils;
+
+import java.util.ArrayList;
 import java.util.List;
 
+import static recomp.RecompVerify.writeErrorTraceFile;
+
 public class RVStrategy implements Runnable {
+    // Global variable for purpose of seeing when threads are done
     public static boolean globalIsDone = false;
 
     private String tla;
@@ -19,33 +31,182 @@ public class RVStrategy implements Runnable {
         this.verbose = verbose;
     }
 
-    synchronized static public boolean isThreadDone() {
+    // returns state of globalIsDone and sets it to true
+    static synchronized public boolean isThreadDone() {
         boolean isDone = globalIsDone;
         globalIsDone = true;
         return isDone;
     }
 
-    public static void runRVStrategies(String tla, String cfg, String recompType, String recompFile, boolean verbose) throws InterruptedException {
-//		ArrayList<Thread> threads = new ArrayList<>();
-        RVStrategy sampleStrategy = new RVStrategy(tla, cfg, recompType, recompFile, verbose);
-        Thread thread = new Thread(sampleStrategy);
-        thread.start();
+    public void runRecompStrategy(final List<String> rawComponents) {
+        String printMsg = "";
+        PerfTimer timer = new PerfTimer();
+        SymbolTable.init();
 
-        try {
-            thread.join();
-        } catch (InterruptedException e) {
-            System.out.println("Thread interrupted while waiting on something.");
+        final String noInvsCfg = "no_invs.cfg";
+
+        // decompose the spec into as many components as possible
+        final List<String> components = Composition.symbolicCompose(tla, cfg, recompType, recompFile, rawComponents);
+        Utils.assertTrue(rawComponents.size() > 0, "Decomposition returned no components");
+        Utils.assertTrue(components.size() > 0, "Symbolic composition returned no components");
+        printMsg = Utils.addPrint(printMsg, "n: " + rawComponents.size());
+        printMsg = Utils.addPrint(printMsg, ("m: " + (components.size() - 1)));
+
+        // model check the first component
+        final String firstComp = components.get(0);
+        printMsg = Utils.addPrintVerbose(printMsg, verbose, "");
+        printMsg = Utils.addPrintVerbose(printMsg, verbose, "Component 1" + ": " + firstComp);
+        TLC tlcFirstComp = new TLC();
+        timer.reset();
+        tlcFirstComp.modelCheckOnlyGoodStates(firstComp, cfg); // TODO there's really no reason to distinguish between the 2 methods
+        printMsg = Utils.addPrintVerbose(printMsg, verbose, "State space gen: " + timer.timeElapsed() + "ms");
+        Utils.assertNotNull(tlcFirstComp.getLTSBuilder(), "Error generating state space for the first component!");
+
+        // turn the first component into a safety property (interface requirement)
+        timer.reset();
+        LTS<Integer, String> ltsProp = tlcFirstComp.getLTSBuilder().toIncompleteDetAutIncludingAnErrorState();
+        printMsg = Utils.addPrintVerbose(printMsg, verbose, "LTS gen: " + timer.timeElapsed() + "ms");
+        printMsg = Utils.addPrintVerbose(printMsg, verbose, "# unique states: " + (ltsProp.size() - 1) + " states");
+        int totalSumOfStatesChecked = ltsProp.size() - 1;
+        int largestProductOfStatesChecked = ltsProp.size() - 1;
+        //System.out.println();
+        //FSPWriter.INSTANCE.write(System.out, ltsProp);
+        //System.out.println();
+
+        // minimize the LTS
+        timer.reset();
+        ltsProp = AutomataLibUtils.minimizeLTS(ltsProp);
+        printMsg = Utils.addPrintVerbose(printMsg, verbose, "minimization: " + timer.timeElapsed() + "ms");
+        printMsg = Utils.addPrintVerbose(printMsg, verbose, "# unique states post-minimization: " + (ltsProp.size() - 1) + " states");
+
+        // initialize the alphabet
+        AlphabetMembershipTester alphabetTester = new AlphabetMembershipTester(tlcFirstComp.actionsInSpec(), ltsProp);
+
+        if (SafetyUtils.INSTANCE.ltsIsSafe(ltsProp)) {
+            final int totalNumStatesChecked = Math.max(totalSumOfStatesChecked, largestProductOfStatesChecked);
+            printMsg = Utils.addPrintVerbose(printMsg, verbose, "");
+            printMsg = Utils.addPrint(printMsg, ("k: " + 0));
+            printMsg = Utils.addPrint(printMsg, ("Total # states checked: " + totalNumStatesChecked));
+            printMsg = Utils.addPrint(printMsg, ("Property satisfied!"));
+            if (!RVStrategy.isThreadDone()) {
+                System.out.print(printMsg);
+            }
+            return;
         }
-        System.exit(99);
+
+        if (SafetyUtils.INSTANCE.hasErrInitState(ltsProp)) {
+            final int totalNumStatesChecked = Math.max(totalSumOfStatesChecked, largestProductOfStatesChecked);
+            printMsg = Utils.addPrintVerbose(printMsg, verbose, "");
+            printMsg = Utils.addPrint(printMsg, ("k: " + 0));
+            printMsg = Utils.addPrint(printMsg, "Total # states checked: " + totalNumStatesChecked);
+            printMsg = Utils.addPrint(printMsg, "Property may be violated.");
+            //FSPWriter.INSTANCE.write(System.out, ltsProp);
+            if (!RVStrategy.isThreadDone()) {
+                System.out.print(printMsg);
+            }
+            return;
+        }
+
+        // at this point, ltsProp represents the interface requirement for the 1st component.
+        // therefore, there is no need to look at the 1st component in the following loop.
+
+        for (int i = 1; i < components.size(); ++i) {
+            final int compNum = i + 1;
+            final String comp = components.get(i);
+            printMsg = Utils.addPrintVerbose(printMsg, verbose, "");
+            printMsg = Utils.addPrintVerbose(printMsg, verbose, "Component " + compNum + ": " + comp);
+
+            TLC tlcComp = new TLC();
+            timer.reset();
+            tlcComp.modelCheck(comp, noInvsCfg, alphabetTester);
+            Utils.addPrintVerbose(printMsg, verbose, "State space gen: " + timer.timeElapsed() + "ms");
+            Utils.assertNotNull(tlcComp.getLTSBuilder(), "Error generating state space for component " + compNum + "!");
+
+            // turn the next component into an LTS (user of the interface provided by ltsProp)
+            timer.reset();
+            LTS<Integer, String> ltsComp = tlcComp.getLTSBuilder().toIncompleteDetAutWithoutAnErrorState();
+            printMsg = Utils.addPrintVerbose(printMsg, verbose, "LTS gen: " + timer.timeElapsed() + "ms");
+            printMsg = Utils.addPrintVerbose(printMsg, verbose, "# unique states: " + (ltsComp.size() - 1) + " states");
+            totalSumOfStatesChecked += ltsComp.size() - 1;
+            //System.out.println();
+            //FSPWriter.INSTANCE.write(System.out, ltsComp);
+            //System.out.println();
+
+            // minimize the LTS for the component
+            timer.reset();
+            ltsComp = AutomataLibUtils.minimizeLTS(ltsComp);
+            printMsg = Utils.addPrintVerbose(printMsg, verbose, "minimization: " + timer.timeElapsed() + "ms");
+            printMsg = Utils.addPrintVerbose(printMsg, verbose, "# unique states post-minimization: " + (ltsComp.size() - 1) + " states");
+            largestProductOfStatesChecked = Math.max(largestProductOfStatesChecked, ltsProp.size() - 1);
+
+            // remove any actions that are now internal to ltsProp
+            // TODO this technically is an abstraction method because later components may have the action
+            //Set<String> ltsPropAlphabet = new HashSet<>(ltsProp.getInputAlphabet());
+            //ltsPropAlphabet.removeAll(ltsComp.getInputAlphabet());
+            //ltsProp = HideUtils.INSTANCE.hideManually(ltsProp, ltsPropAlphabet);
+
+            // create new safety property (interface requirement for all components seen so far)
+            timer.reset();
+            ltsProp = ParallelComposition.INSTANCE.parallel(ltsComp, ltsProp);
+            Utils.printVerbose(verbose, "New property gen (|| composition): " + timer.timeElapsed() + "ms");
+
+            // collect the alphabet
+            alphabetTester.update(tlcComp.actionsInSpec(), ltsProp);
+
+            // if the new safety property is TRUE or FALSE then model checking is done
+            if (SafetyUtils.INSTANCE.ltsIsSafe(ltsProp)) {
+                final int totalNumStatesChecked = Math.max(totalSumOfStatesChecked, largestProductOfStatesChecked);
+                printMsg = Utils.addPrint(printMsg, "");
+                printMsg = Utils.addPrint(printMsg, "k: " + i);
+                printMsg = Utils.addPrint(printMsg, "Total # states checked: " + totalNumStatesChecked);
+                printMsg = Utils.addPrint(printMsg, "Property satisfied!");
+                if (!RVStrategy.isThreadDone()) {
+                    System.out.print(printMsg);
+                }
+                return;
+            }
+            if (SafetyUtils.INSTANCE.hasErrInitState(ltsProp)) {
+                final int totalNumStatesChecked = Math.max(totalSumOfStatesChecked, largestProductOfStatesChecked);
+                Utils.printVerbose(verbose, "");
+                printMsg = Utils.addPrint(printMsg, "k: " + i);
+                printMsg = Utils.addPrint(printMsg, "Total # states checked: " + totalNumStatesChecked);
+                printMsg = Utils.addPrint(printMsg, "Property may be violated.");
+                //FSPWriter.INSTANCE.write(System.out, ltsProp);
+                if (!RVStrategy.isThreadDone()) {
+                    System.out.print(printMsg);
+                }
+                return;
+            }
+        }
+
+        final int totalNumStatesChecked = Math.max(totalSumOfStatesChecked, largestProductOfStatesChecked);
+        printMsg = Utils.addPrintVerbose(printMsg, verbose, "");
+        printMsg = Utils.addPrint(printMsg, "k: " + (components.size() - 1));
+        printMsg = Utils.addPrint(printMsg, "Total # states checked: " + totalNumStatesChecked);
+        printMsg = Utils.addPrint(printMsg, "Property may be violated.");
+
+        //Output everything
+        if (!RVStrategy.isThreadDone()) {
+            System.out.print(printMsg);
+        }
+
+        // encode the sequence of actions that leads to an error in a new TLA+ file	// encode the sequence of actions that leads to an error in a new TLA+ file
+        // TODO write error trace for early termination	// TODO write error trace for early termination
+        writeErrorTraceFile(tla, cfg, ltsProp);
+
+        // it should produce an error trace	// it should produce an error trace
     }
 
+    // Must implement run() for Runnable (threads)
     @ Override
     public void run() {
+        // Each tread performs runRecompStrategy with the given instance variables of the instantiated class
         try {
             List<String> rawComponents = Decomposition.decompAll(tla, cfg);
-            RecompVerify.runRecompMap(tla, cfg, recompType, recompFile, verbose, rawComponents);
+            runRecompStrategy(rawComponents);
         } catch (Exception e) {
             System.out.println("Thread interrupted while waiting on something.");
         }
+
     }
 }
--- tlatools/org.lamport.tlatools/src/recomp/RecompVerify.java
+++ tlatools/org.lamport.tlatools/src/recomp/RecompVerify.java
@@ -53,167 +53,24 @@ public class RecompVerify {
 		final String noInvsCfg = "no_invs.cfg";
 		Utils.writeFile(noInvsCfg, "SPECIFICATION Spec");
 
+		// Make ArrayList of threads and strategies. Join threads which runs all of them
 		try {
-			RVStrategy.runRVStrategies(tla, cfg, recompType, recompFile, verbose);
+	//		ArrayList<Thread> threads = new ArrayList<>();
+	//      ArrayList<RVStrategy> strategies = new ArrayList<>();
+			RVStrategy sampleStrategy = new RVStrategy(tla, cfg, recompType, recompFile, verbose);
+			Thread thread = new Thread(sampleStrategy);
+			thread.start();
+			thread.join();
 		} catch (InterruptedException e) {
+			System.out.println("Thread interrupted while waiting on something.");
 			throw new RuntimeException(e);
 		}
 
+		// not unix convention, but we use this to signal to the wrapper script that	// not unix convention, but we use this to signal to the wrapper script that
 		System.exit(99);
 	}
 
-	public static void runRecompMap(final String tla, final String cfg, final String recompType, final String recompFile, boolean verbose, final List<String> rawComponents) {
-		String printMsg = "";
-		PerfTimer timer = new PerfTimer();
-		SymbolTable.init();
-
-		final String noInvsCfg = "no_invs.cfg";
-
-		// decompose the spec into as many components as possible
-		final List<String> components = Composition.symbolicCompose(tla, cfg, recompType, recompFile, rawComponents);
-		Utils.assertTrue(rawComponents.size() > 0, "Decomposition returned no components");
-		Utils.assertTrue(components.size() > 0, "Symbolic composition returned no components");
-		printMsg = Utils.addPrint(printMsg, "n: " + rawComponents.size());
-		printMsg = Utils.addPrint(printMsg, ("m: " + (components.size() - 1)));
-
-		// model check the first component
-		final String firstComp = components.get(0);
-		printMsg = Utils.addPrintVerbose(printMsg, verbose, "");
-		printMsg = Utils.addPrintVerbose(printMsg, verbose, "Component 1" + ": " + firstComp);
-		TLC tlcFirstComp = new TLC();
-		timer.reset();
-		tlcFirstComp.modelCheckOnlyGoodStates(firstComp, cfg); // TODO there's really no reason to distinguish between the 2 methods
-		printMsg = Utils.addPrintVerbose(printMsg, verbose, "State space gen: " + timer.timeElapsed() + "ms");
-		Utils.assertNotNull(tlcFirstComp.getLTSBuilder(), "Error generating state space for the first component!");
-
-		// turn the first component into a safety property (interface requirement)
-		timer.reset();
-		LTS<Integer, String> ltsProp = tlcFirstComp.getLTSBuilder().toIncompleteDetAutIncludingAnErrorState();
-		printMsg = Utils.addPrintVerbose(printMsg, verbose, "LTS gen: " + timer.timeElapsed() + "ms");
-		printMsg = Utils.addPrintVerbose(printMsg, verbose, "# unique states: " + (ltsProp.size()-1) + " states");
-		int totalSumOfStatesChecked = ltsProp.size() - 1;
-		int largestProductOfStatesChecked = ltsProp.size() - 1;
-		//System.out.println();
-		//FSPWriter.INSTANCE.write(System.out, ltsProp);
-		//System.out.println();
-
-		// minimize the LTS
-		timer.reset();
-		ltsProp = AutomataLibUtils.minimizeLTS(ltsProp);
-		printMsg = Utils.addPrintVerbose(printMsg, verbose, "minimization: " + timer.timeElapsed() + "ms");
-		printMsg = Utils.addPrintVerbose(printMsg, verbose, "# unique states post-minimization: " + (ltsProp.size()-1) + " states");
-
-		// initialize the alphabet
-		AlphabetMembershipTester alphabetTester = new AlphabetMembershipTester(tlcFirstComp.actionsInSpec(), ltsProp);
-
-		if (SafetyUtils.INSTANCE.ltsIsSafe(ltsProp)) {
-			final int totalNumStatesChecked = Math.max(totalSumOfStatesChecked, largestProductOfStatesChecked);
-			printMsg = Utils.addPrintVerbose(printMsg, verbose, "");
-			printMsg = Utils.addPrint(printMsg, ("k: " + 0));
-			printMsg = Utils.addPrint(printMsg, ("Total # states checked: " + totalNumStatesChecked));
-			printMsg = Utils.addPrint(printMsg, ("Property satisfied!"));
-			if (!RVStrategy.isThreadDone()) System.out.print(printMsg);
-			return;
-		}
-
-		if (SafetyUtils.INSTANCE.hasErrInitState(ltsProp)) {
-			final int totalNumStatesChecked = Math.max(totalSumOfStatesChecked, largestProductOfStatesChecked);
-			printMsg = Utils.addPrintVerbose(printMsg, verbose, "");
-			printMsg = Utils.addPrint(printMsg, ("k: " + 0));
-			printMsg = Utils.addPrint(printMsg, "Total # states checked: " + totalNumStatesChecked);
-			printMsg = Utils.addPrint(printMsg, "Property may be violated.");
-			//FSPWriter.INSTANCE.write(System.out, ltsProp);
-			if (!RVStrategy.isThreadDone())
-				System.out.print(printMsg);
-			return;
-		}
-
-		// at this point, ltsProp represents the interface requirement for the 1st component.
-		// therefore, there is no need to look at the 1st component in the following loop.
-
-		for (int i = 1; i < components.size(); ++i) {
-			final int compNum = i + 1;
-			final String comp = components.get(i);
-			printMsg = Utils.addPrintVerbose(printMsg, verbose, "");
-			printMsg = Utils.addPrintVerbose(printMsg,verbose, "Component " + compNum + ": " + comp);
-
-			TLC tlcComp = new TLC();
-			timer.reset();
-			tlcComp.modelCheck(comp, noInvsCfg, alphabetTester);
-			Utils.addPrintVerbose(printMsg,verbose, "State space gen: " + timer.timeElapsed() + "ms");
-			Utils.assertNotNull(tlcComp.getLTSBuilder(), "Error generating state space for component " + compNum + "!");
-
-			// turn the next component into an LTS (user of the interface provided by ltsProp)
-			timer.reset();
-			LTS<Integer, String> ltsComp = tlcComp.getLTSBuilder().toIncompleteDetAutWithoutAnErrorState();
-			printMsg = Utils.addPrintVerbose(printMsg,verbose, "LTS gen: " + timer.timeElapsed() + "ms");
-			printMsg = Utils.addPrintVerbose(printMsg,verbose, "# unique states: " + (ltsComp.size()-1) + " states");
-			totalSumOfStatesChecked += ltsComp.size() - 1;
-			//System.out.println();
-			//FSPWriter.INSTANCE.write(System.out, ltsComp);
-			//System.out.println();
-
-			// minimize the LTS for the component
-			timer.reset();
-			ltsComp = AutomataLibUtils.minimizeLTS(ltsComp);
-			printMsg = Utils.addPrintVerbose(printMsg, verbose,"minimization: " + timer.timeElapsed() + "ms");
-			printMsg = Utils.addPrintVerbose(printMsg, verbose,"# unique states post-minimization: " + (ltsComp.size()-1) + " states");
-			largestProductOfStatesChecked = Math.max(largestProductOfStatesChecked, ltsProp.size()-1);
-
-			// remove any actions that are now internal to ltsProp
-			// TODO this technically is an abstraction method because later components may have the action
-			//Set<String> ltsPropAlphabet = new HashSet<>(ltsProp.getInputAlphabet());
-			//ltsPropAlphabet.removeAll(ltsComp.getInputAlphabet());
-			//ltsProp = HideUtils.INSTANCE.hideManually(ltsProp, ltsPropAlphabet);
-
-			// create new safety property (interface requirement for all components seen so far)
-			timer.reset();
-			ltsProp = ParallelComposition.INSTANCE.parallel(ltsComp, ltsProp);
-			Utils.printVerbose(verbose, "New property gen (|| composition): " + timer.timeElapsed() + "ms");
-
-			// collect the alphabet
-			alphabetTester.update(tlcComp.actionsInSpec(), ltsProp);
-
-			// if the new safety property is TRUE or FALSE then model checking is done
-			if (SafetyUtils.INSTANCE.ltsIsSafe(ltsProp)) {
-				final int totalNumStatesChecked = Math.max(totalSumOfStatesChecked, largestProductOfStatesChecked);
-				printMsg = Utils.addPrint(printMsg,  "");
-				printMsg = Utils.addPrint(printMsg, "k: " + i);
-				printMsg = Utils.addPrint(printMsg,"Total # states checked: " + totalNumStatesChecked);
-				printMsg = Utils.addPrint(printMsg,"Property satisfied!");
-				if (!RVStrategy.isThreadDone()) System.out.print(printMsg);
-				return;
-			}
-			if (SafetyUtils.INSTANCE.hasErrInitState(ltsProp)) {
-				final int totalNumStatesChecked = Math.max(totalSumOfStatesChecked, largestProductOfStatesChecked);
-				Utils.printVerbose(verbose, "");
-				printMsg = Utils.addPrint(printMsg,"k: " + i);
-				printMsg = Utils.addPrint(printMsg,"Total # states checked: " + totalNumStatesChecked);
-				printMsg = Utils.addPrint(printMsg,"Property may be violated.");
-				//FSPWriter.INSTANCE.write(System.out, ltsProp);
-				if (!RVStrategy.isThreadDone()) System.out.print(printMsg);
-				return;
-			}
-		}
-
-		final int totalNumStatesChecked = Math.max(totalSumOfStatesChecked, largestProductOfStatesChecked);
-		printMsg = Utils.addPrintVerbose(printMsg, verbose, "");
-		printMsg = Utils.addPrint(printMsg,"k: " + (components.size() - 1));
-		printMsg = Utils.addPrint(printMsg,"Total # states checked: " + totalNumStatesChecked);
-		printMsg = Utils.addPrint(printMsg,"Property may be violated.");
-
-		//Output everything
-		if (!RVStrategy.isThreadDone()) System.out.print(printMsg);
-
-		// encode the sequence of actions that leads to an error in a new TLA+ file	// encode the sequence of actions that leads to an error in a new TLA+ file
-		// TODO write error trace for early termination	// TODO write error trace for early termination
-		writeErrorTraceFile(tla, cfg, ltsProp);	writeErrorTraceFile(tla, cfg, ltsProp);
-
-		// not unix convention, but we use this to signal to the wrapper script that	// not unix convention, but we use this to signal to the wrapper script that
-		// it should produce an error trace	// it should produce an error trace
-	}
-
-	private static void writeErrorTraceFile(final String tla, final String cfg, final LTS<Integer, String> ltsProp) {
+	static void writeErrorTraceFile(final String tla, final String cfg, final LTS<Integer, String> ltsProp) {
 		final Word<String> trace = SafetyUtils.INSTANCE.findErrorTrace(ltsProp);
 		/*System.out.println("Error trace:");
 		for (final String act : trace) {
