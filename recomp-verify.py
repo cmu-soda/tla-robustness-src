5ef5c6554fc0599708e61001720fa856325e7adf
eduardoloz <eddieslozano@gmail.com>
2024-09-18 12:18:34 -0400

    Most recent commit from python. Includes parallel bash command with the halt flag

--- recomp-verify.py
+++ recomp-verify.py
@@ -1,7 +1,6 @@
 import concurrent.futures
 import glob
 import os
-import signal
 import shutil
 import subprocess
 import sys
@@ -11,12 +10,9 @@ root_dir = os.path.dirname(os.path.abspath(__file__))
 tool = root_dir + "/bin/recomp-verify.jar"
 tlc = root_dir + "/bin/tla2tools.jar"
 
-# some change Eduardo is testing
-
 def write(name, contents):
-    f = open(name, "w")
-    f.write(contents)
-    f.close()
+    with open(name, "w") as f:
+        f.write(contents)
 
 def decomp(spec, cfg):
     cmd_args = ["java", "-jar", tool, spec, cfg, "--decomp"]
@@ -28,11 +24,11 @@ def create_err_trace(txt):
     keep = []
     capture = False
     for l in lines:
-        if ("Error:" in l):
+        if "Error:" in l:
             capture = True
-        if ("distinct states found" in l):
+        if "distinct states found" in l:
             capture = False
-        if (capture and "errCounter" not in l):
+        if capture and "errCounter" not in l:
             keep.append(l)
     return "\n".join(keep)
 
@@ -49,11 +45,11 @@ def verify(spec, cfg, cust, naive, verbose):
         cmd_args.append("--verbose")
     retcode = subprocess.call(cmd_args)
 
-    if (retcode == 99):
+    if retcode == 99:
         replay_args = ["java", "-jar", tlc, "-deadlock", "ErrTrace.tla"]
         replay = subprocess.run(replay_args, capture_output=True, text=True)
         replay_out = replay.stdout
-        if ("Error:" in replay_out):
+        if "Error:" in replay_out:
             err_trace = create_err_trace(replay_out)
             print("Here is the error trace:\n")
             print(err_trace)
@@ -65,6 +61,7 @@ def verify_single_process(spec, cfg, cust, naive, verbose):
     os.makedirs("out", exist_ok=True)
     dest_dir = orig_dir + "/out"
 
+
     #shutil.copyfile(spec, "out/"+spec)
     #shutil.copyfile(cfg, "out/"+cfg)
     for filename in glob.glob(os.path.join(orig_dir, '*.*')):
@@ -75,8 +72,8 @@ def verify_single_process(spec, cfg, cust, naive, verbose):
     os.chdir(orig_dir)
 
 def verify_capture_output(spec, cfg, pdir, *args):
-    shutil.copy(spec, pdir+"/")
-    shutil.copy(cfg, pdir+"/")
+    shutil.copy(spec, pdir + "/")
+    shutil.copy(cfg, pdir + "/")
     #os.chdir(pdir)
     #write("out.log","")
 
@@ -87,41 +84,47 @@ def verify_capture_output(spec, cfg, pdir, *args):
     # uses TLC to run the one-component case
     if pdir == "mono":
         cmd_args = ["java", "-Xmx7g", "-jar", tlc, "-deadlock", "-workers", "1", "-config", cfg, spec]
-
+   
     #naive_log = open("out.log","w")
     #process = subprocess.Popen(cmd_args, stdout=naive_log.fileno())
     #process.wait()
     #return open("naive/out.log").read().rstrip()
     #ret = subprocess.run(cmd_args, capture_output=True, text=True)
-
+    
     cd_args = ["cd", pdir]
     cmd = " ".join(cd_args) + "; " + " ".join(cmd_args)
     ret = subprocess.run(cmd, capture_output=True, text=True, shell=True)
 
     return ret.stdout.rstrip()
 
-def run_multi_verif(dest_dir, spec, cfg):
+def run_multi_verif(dest_dir, spec, cfg, halt=False):
     executor = concurrent.futures.ThreadPoolExecutor(max_workers=8)
     futures = []
-
+    
     # spawn a process for the naive map
     f = partial(verify_capture_output, spec, cfg, "naive", "--naive")
     futures.append(executor.submit(f))
-
+    
     # spawn a process for each recomp map
     pdirs = ["mono", "cust_1", "cust_2"]
     for pdir in pdirs:
         f = partial(verify_capture_output, spec, cfg, pdir, "--cust", "custom_recomp.csv")
         futures.append(executor.submit(f))
+    
+    done, not_done = concurrent.futures.wait(futures, return_when=concurrent.futures.FIRST_COMPLETED if halt else concurrent.futures.ALL_COMPLETED)
 
-    done, not_done = concurrent.futures.wait(futures, return_when=concurrent.futures.FIRST_COMPLETED)
+    # we cancel the ones that have not finished
+    if halt:
+        for future in not_done:
+            future.cancel()
+
     winner = done.pop()
     output = winner.result()
     #for future in not_done:
-    #future.cancel()
+        #future.cancel()
     return output
 
-def verify_multi_process(spec, cfg, verbose):
+def verify_multi_process(spec, cfg, verbose, halt=False):
     orig_dir = os.getcwd()
     os.makedirs("out", exist_ok=True)
     dest_dir = orig_dir + "/out"
@@ -143,7 +146,6 @@ def verify_multi_process(spec, cfg, verbose):
         os.makedirs("mono", exist_ok=True)
         output = verify_capture_output(spec, cfg, "mono")
         print(output)
-        print(".")
         return
 
     # otherwise, build three recomp maps
@@ -157,83 +159,27 @@ def verify_multi_process(spec, cfg, verbose):
     write("cust_1/custom_recomp.csv", recomp_1)
     write("cust_2/custom_recomp.csv", recomp_2)
 
-    # also run the naive mapping
     os.makedirs("naive", exist_ok=True)
 
-    output = run_multi_verif(dest_dir, spec, cfg)
+    output = run_multi_verif(dest_dir, spec, cfg, halt=halt)
     print(output)
-    print(".")
-
-def recomp_verify_paralleljava(spec, cfg):
-    # Prepare directories and cleanup
-    orig_dir = os.getcwd()
-    os.makedirs("out", exist_ok=True)
-    dest_dir = orig_dir + "/out"
-    shutil.copy(spec, dest_dir)
-    shutil.copy(cfg, dest_dir)
-    os.chdir("out")
-
-    # Decompose the specification
-    os.makedirs("decomp", exist_ok=True)
-    shutil.copy(spec, "decomp/")
-    shutil.copy(cfg, "decomp/")
-    os.chdir("decomp")
-    components = decomp(spec, cfg)
-    os.chdir(dest_dir)
-
-    # if there's only one component then there is no need to run multiple processes
-    if len(components) <= 1:
-        os.makedirs("mono", exist_ok=True)
-        output = verify_capture_output(spec, cfg, "mono")
-        print(output)
-        print(".")
-        return
-
-    # otherwise, build three recomp maps
-    os.makedirs("mono", exist_ok=True)
-    os.makedirs("cust_1", exist_ok=True)
-    os.makedirs("cust_2", exist_ok=True)
-    mono = ",".join(components) + "\n"
-    recomp_1 = components[0] + "\n" + ",".join(components[1:]) + "\n"
-    recomp_2 = ",".join(components[0:-1]) + "\n" + components[-1] + "\n"
-    write("mono/custom_recomp.csv", mono)
-    write("cust_1/custom_recomp.csv", recomp_1)
-    write("cust_2/custom_recomp.csv", recomp_2)
-
-    # also run the naive mapping
-    os.makedirs("naive", exist_ok=True)
-
-    # Run the Java recomp-verify tool with the provided spec and cfg
-    cmd_args = ["java", "-jar", tool, spec, cfg]
-    subprocess.run(cmd_args)
 
 def run():
-    if (len(sys.argv) < 3 or len(sys.argv) > 5):
-        print("usage: recomp-verify.py <file.tla> <file.cfg> [--cust|--naive|--parallel|--parallel_java] [--verbose]")
+    if len(sys.argv) < 3 or len(sys.argv) > 6:
+        print("usage: recomp-verify.py <file.tla> <file.cfg> [--cust|--naive|--parallel|--halt] [--verbose]")
         return
-    if (len(sys.argv) > 3 and sys.argv[3] != "--parallel" and sys.argv[3] != "--parallel_java"):
-        if (len(sys.argv) == 4 and sys.argv[3] != "--cust" and sys.argv[3] != "--naive" and sys.argv[3] != "--verbose"):
-            print("usage: recomp-verify.py <file.tla> <file.cfg> [--cust|--naive|--parallel|--parallel_java] [--verbose]")
-            return
-        if (len(sys.argv) == 5 and sys.argv[4] != "--cust" and sys.argv[4] != "--naive" and sys.argv[4] != "--verbose"):
-            print("usage: recomp-verify.py <file.tla> <file.cfg> [--cust|--naive|--parallel|--parallel_java] [--verbose]")
-            return
 
     spec = sys.argv[1]
     cfg = sys.argv[2]
-    multi_process_java = len(sys.argv) >= 4 and sys.argv[3] == "--parallel_java"
-    multi_process = len(sys.argv) >= 4 and sys.argv[3] == "--parallel"
-    cust = (len(sys.argv) >= 4 and sys.argv[3] == "--cust") or (len(sys.argv) >= 5 and sys.argv[4] == "--cust")
-    naive = (len(sys.argv) >= 4 and sys.argv[3] == "--naive") or (len(sys.argv) >= 5 and sys.argv[4] == "--naive")
-    verbose = (len(sys.argv) >= 4 and sys.argv[3] == "--verbose") or (len(sys.argv) >= 5 and sys.argv[4] == "--verbose")
+    multi_process = "--parallel" in sys.argv
+    cust = "--cust" in sys.argv
+    naive = "--naive" in sys.argv
+    verbose = "--verbose" in sys.argv
+    halt = "--halt" in sys.argv
 
     if multi_process:
-        verify_multi_process(spec, cfg, verbose)
-    elif multi_process_java:
-        recomp_verify_paralleljava(spec, cfg)
+        verify_multi_process(spec, cfg, verbose, halt=halt)
     else:
         verify_single_process(spec, cfg, cust, naive, verbose)
 
 run()
-# force exit. this will leave zombie processes running for a short period of time, which is not ideal
-os._exit(0)
